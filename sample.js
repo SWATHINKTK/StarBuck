b10460f3987bca0dee22be8596d446ae4667c3b2
Swathin K T K <swathinktk10@gmail.com>
2023-08-17 18:17:45 +0530

    Starbucks

---
+++ sample.js
@@ -0,0 +1,20 @@
+document.getElementById("signin").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/account/signin?ReturnUrl=%2F"
+} )
+document.getElementById("join").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/account/create"
+} )
+document.getElementById("Explore").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/rewards/mobile-apps/?_branch_match_id=1218427776889376195&utm_source=marketing&utm_campaign=summer_remix&utm_medium=Marketing&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXLy5JLEoqTc4u1kssKNDLyczL1g%2FOTjYKsXCpSndOAgBEcj0YJgAAAA%3D%3D"
+} )
+
+document.getElementById("order1").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/menu/drinks/cold-drinks"
+} )
+document.getElementById("order2").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/menu/product/2123644/iced"
+} )
+document.getElementById("order3").addEventListener("click",function(){
+        window.location.href="https://www.starbucks.com/menu/product/2123236/single"
+} )
+
